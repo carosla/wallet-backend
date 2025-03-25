0d5febe5119a4efe7fa1b04c8ef99670d831bc45
Ana Carolina <anacarolinaneias@gmail.com>
2025-03-25 09:47:52 -0300

    Adicionando cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,11 @@ const transactionRoutes = require("./routes/transactionRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["*"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
