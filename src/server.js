790a7a87895080b4a3926fc06082da27dbd06d8e
Ana Carolina <anacarolinaneias@gmail.com>
2025-03-25 10:36:56 -0300

    Teste de origin

--- src/server.js
+++ src/server.js
@@ -12,12 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8081"); // Define a origem permitida
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); 
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Retorna 200 para requisições OPTIONS (preflight)
+    return res.status(200).end(); 
   }
 
   next();
