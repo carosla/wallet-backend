628b7c6a0b1923f146dda511b76feeed2680ed59
carosla <anacarolinaneias@gmail.com>
2025-03-29 20:28:12 -0300

    Ajustando associations

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,12 +1,25 @@
 const TipoTransacao = require("./tipo_transacao");
 const Transacoes = require("./transacoes");
+const Categoria = require('./categoria');
 
+// Uma categoria pode ter várias transações
+Categoria.hasMany(Transacoes, {
+  foreignKey: "categoria_id",
+});
+
+// Um tipo de transação (entrada/saída) pode ter várias transações
 TipoTransacao.hasMany(Transacoes, {
   foreignKey: "tipo_transacao_id",
 });
 
+// Cada transação pertence a uma categoria
+Transacoes.belongsTo(Categoria, {
+  foreignKey: "categoria_id",
+});
+
+// Cada transação pertence a um tipo de transação (entrada/saída)
 Transacoes.belongsTo(TipoTransacao, {
   foreignKey: "tipo_transacao_id",
 });
 
-module.exports = { TipoTransacao, Transacoes };
+module.exports = { TipoTransacao, Transacoes, Categoria };
