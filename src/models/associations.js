da809664ee9b48e244205f3d6edece3ac4aad566
carosla <anacarolinaneias@gmail.com>
2025-05-18 19:49:09 -0300

    alterações na busca do perfil

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,6 +1,7 @@
 const TipoTransacao = require("./tipo_transacao");
 const Transacoes = require("./transacoes");
 const Categoria = require('./categoria');
+const Usuario = require('./usuario')
 
 // Uma categoria pode ter várias transações
 Categoria.hasMany(Transacoes, {
@@ -20,6 +21,10 @@ Transacoes.belongsTo(Categoria, {
 // Cada transação pertence a um tipo de transação (entrada/saída)
 Transacoes.belongsTo(TipoTransacao, {
   foreignKey: "tipo_transacao_id",
+});
+
+Usuario.hasOne(Perfil, {
+  foreignKey: "usuario_id",
 });
 
 
